9db507e42be1f91a2469fc1906aa9b742d969124
Petrov Dmitry <dima000killer@gmail.com>
2023-04-15 17:58:46 +0500

    Создана лента без отзывов

---
+++ Day check/src/ContactBook.java
@@ -0,0 +1,40 @@
+// Ограничьте класс ContactBook так, чтобы он мог хранить в себе только список контактов
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class ContactBook<T extends Contact> {
+    // Объявите поле класса contacts - список контактов книги
+    ArrayList <T> contacts = new ArrayList<>();
+
+    public void addContact(T contact) {
+        contacts.add(contact);
+    }
+
+    public void printList() {
+        // Выведите на экран весь список контактов книги
+        for (T contact : contacts) {
+            System.out.println("Имя: " + contact.getName());
+            contact.print();
+        }
+
+    }
+
+    public void congratulate(String name) {
+        boolean contactPresented = false; //проверяем есть ли контакт в базе
+        // Найдите контакт в книге по имени, и отправьте ему сообщение с помощью метода sendMessage()
+        for (T contact : contacts) {
+            if (Objects.equals(contact.getName(), name) && contact.getName() != null) {
+                System.out.println("Поздравим с Новым годом ваш контакт из записной книжки: " + name);
+                contact.sendMessage();
+                contactPresented = true;
+            }
+        }
+        if (!contactPresented) {
+            // Если контакт не найден, выведите соответсвующее сообщение
+            System.out.println("Не найден контакт с указанным именем.");
+        }
+
+
+    }
+
+}
